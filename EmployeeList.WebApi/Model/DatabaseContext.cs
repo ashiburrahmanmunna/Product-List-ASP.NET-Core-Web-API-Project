74031fa87e6e20ccd43b9ee17b431210973e5209
Ashibur Rahman Munna <114874876+ashiburrahmanmunna@users.noreply.github.com>
2023-03-05 14:38:29 +0600

    Add files via upload

---
+++ EmployeeList.WebApi/Model/DatabaseContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace EmployeeList.WebApi.Model
+{
+    public partial class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee>? Employees { get; set; }
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+                entity.Property(e => e.ID).HasColumnName("ID");
+                entity.Property(e => e.CategoryName).HasMaxLength(15).IsUnicode(false);
+                entity.Property(e => e.UnitName).HasMaxLength(100).IsUnicode(false);
+                entity.Property(e => e.Name).HasMaxLength(256).IsUnicode(false);
+                entity.Property(e => e.Code).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.ParentCode).IsUnicode(false);
+                entity.Property(e => e.ProductBarcode).HasMaxLength(1).IsUnicode(false);
+                entity.Property(e => e.Description).HasMaxLength(1).IsUnicode(false);
+                entity.Property(e => e.BrandName).IsUnicode(false);
+                entity.Property(e => e.SizeName).IsUnicode(false);
+                entity.Property(e => e.ColorName).IsUnicode(false);
+                entity.Property(e => e.ModelName).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.VariantName).IsUnicode(false);
+                entity.Property(e => e.OldPrice).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.Price).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.CostPrice).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.WarehouseList).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.stock).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.TotalPurchase).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.LastPurchaseDate).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.LastPurchaseSupplier).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.TotalSales).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.LastSalesDate).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.LastSalesCustomer).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.ImagePath).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.Type).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.Status).HasMaxLength(50).IsUnicode(false);
+            });
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ EmployeeList.WebApi/Model/Employee.cs
@@ -0,0 +1,33 @@
+﻿namespace EmployeeList.WebApi.Model
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string? CategoryName { get; set; }
+        public string? UnitName { get; set; }
+        public string? Name { get; set; }
+        public string? Code { get; set; }
+        public string? ParentCode { get; set; }
+        public string? ProductBarcode { get; set; }
+        public string? Description { get; set; }
+        public string? BrandName { get; set; }
+        public string? SizeName { get; set; }
+        public string? ColorName { get; set; }
+        public string? ModelName { get; set; }
+        public string? VariantName { get; set; }
+        public int OldPrice { get; set; }
+        public int Price { get; set; }
+        public int CostPrice { get; set; }
+        public string? WarehouseList { get; set; }
+        public int stock { get; set; }
+        public int TotalPurchase { get; set; }
+        public DateTime LastPurchaseDate { get; set; }
+        public string? LastPurchaseSupplier { get; set; }
+        public int TotalSales { get; set; }
+        public DateTime LastSalesDate { get; set; }
+        public string? LastSalesCustomer { get; set; }
+        public string? ImagePath { get; set; }
+        public string? Type { get; set; }
+        public string? Status { get; set; }
+    }
+}
